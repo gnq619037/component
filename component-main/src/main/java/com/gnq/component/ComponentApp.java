dde0a2a7d5d1e177b886b3cb601bbe2f9f6a82b6
guonq@quantdo.com.cn <guonq@quantdo.com.cn>
2020-06-11 18:40:03 +0800

    测试接口

---
+++ component-main/src/main/java/com/gnq/component/ComponentApp.java
@@ -0,0 +1,20 @@
+package com.gnq.component;/**
+ * @author guonanqing
+ * @date 2020/6/11 18:18
+ * @version 1.0
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: guonanqing
+ * @desc:
+ * @date: 2020/6/11
+ */
+@SpringBootApplication
+public class ComponentApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ComponentApp.class, args);
+    }
+}
